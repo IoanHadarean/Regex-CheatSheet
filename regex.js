42a66e0c10cc32eee4e29e85804fdb5f44184ded
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-05-09 19:09:24 +0000

    Added regex cheat sheet part two

--- regex.js
+++ regex.js
@@ -24,19 +24,35 @@ re = /[0-9][0-9]ray/;       // Match any digit
 
 
 // Braces {}  - Quantifiers
-re = /Hel{2}o/i;         // {n}    - n = amount of times
-re = /Hel{2,4}o/i;       // {n, m}  -  n <=  amount of times  <= m
-re = /Hel{2,}o/i;       // {n,}   - n <= amount of times
+re = /Hel{2}o/i;         // {n}    - n = amount of times character before braces occurs
+re = /Hel{2,4}o/i;       // {n, m}  -  n <=  amount of times character before braces occurs  <= m
+re = /Hel{2,}o/i;       // {n,}   - n <= amount of times character before braces occurs
 
 
 // Parentheses ()  - Grouping
 re = /^([0-9]x){3}$/;
 
+// Shorthand Character Classes
+// Note: empty string is not a match
+re = /\w/;       // Word character -  alphanumeric or _
+re = /\w+/;      // +  means one or more
+re = /\W/;       // Non-Word character (not alphanumeric or _)
+re = /\d/;       // Match any digit
+re = /\d+/;       // Match any digit 0 or more times
+re = /\D/;        // Match any non-digit 
+re = /\s/;        // Match whitespace char
+re = /\S/;        // Match non-whitespace char
+re = /pogger\b/i;  // Word boundary
+
+
+// Assertions
+re = /x(?=y)/;   // Match x only if followed by y
+re = /x(?!y)/;   // Match x only if NOT followed by y
 
 
 
 // String to match
-const str = '3x3x3x';
+const str = 'Poggers, welcome to Pogger Land';
 
 // Log results
 const result = re.exec(str);
